487f02de47611145dcb5d07645b16f6c72207e16
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-07-22 11:29:17 -0400

    Implement call, return, and add functions

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -13,16 +13,19 @@ class CPU:
 
         # Pointers
         self.pc = 0
-        self.sp = 0xF3
+        self.sp = 7
 
         # Reference Table
         self.branchtable = {
             0b00000001: self.HLT,
             0b10000010: self.LDI,
             0b01000111: self.PRN,
+            0b10100000: self.ADD,
             0b10100010: self.MUL,
             0b01000101: self.PUSH,
-            0b01000110: self.POP
+            0b01000110: self.POP,
+            0b01010000: self.CALL,
+            0b00010001: self.RET
         }
     
     def ram_read(self, MAR):
@@ -41,6 +44,10 @@ class CPU:
     def PRN(self, a, b):
         print(self.registers[a])
         self.pc += 2
+    
+    def ADD(self, a, b):
+        self.alu("ADD", a, b)
+        self.pc += 3
 
     def MUL(self, a, b):
         self.alu("MUL", a, b)
@@ -56,6 +63,17 @@ class CPU:
         self.ram[self.sp] = 0
         self.sp += 1
         self.pc += 2
+    
+    def CALL(self, a, b):
+        return_address = self.pc + 2
+        self.registers[self.sp] -= 1
+        self.ram[self.registers[self.sp]] = return_address
+        self.pc = self.registers[a]
+
+    def RET(self, a, b):
+        return_address = self.ram[self.registers[self.sp]]
+        self.registers[self.sp] += 1
+        self.pc = return_address
 
     def load(self, file):
         """Load a program into memory."""
